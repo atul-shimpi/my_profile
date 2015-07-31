f7516fd5da395508f36cca47c51bea00cc651cb4
Atul Shimpi <shimpiatul@hotmail.com>
2015-07-31 16:18:37 +0530

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   root 'home#index'
+  #get 'app', to: redirect('/')
+
+
+
+  #get 'app', to: redirect { |params, request|
+   #          puts "Helloworld-2 #{params}"
+    #    path = 'app/'
+     #   "http://#{request.host_with_port}/#{path}"
+      # }
+
+
+
+  #get '/index', to: redirect('/index/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
