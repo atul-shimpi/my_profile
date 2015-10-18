505e2c2aff31901f5262c00a9b825e406def4ce7
atul-shimpi <shimpiatul@hotmail.com>
2015-10-18 20:06:18 +0530

    Read HTTP_X_FORWARDED_FOR

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
   	RequestHeader.create(remote_ip: request.headers[:REMOTE_ADDR],
-												 remote_host: request.headers[:REMOTE_IP],
+												 remote_host: request.headers[:HTTP_X_FORWARDED_FOR],
 												 server_name: request.headers[:SERVER_NAME]
 												)	
   end
--- app/controllers/request_headers_controller.rb
+++ app/controllers/request_headers_controller.rb
@@ -1,5 +1,6 @@
 class RequestHeadersController < ApplicationController
 	def index
 		@request_headers = RequestHeader.all()
+    @request = request.headers
 	end
 end
