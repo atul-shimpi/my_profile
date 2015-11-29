27fec896e654020724189ba78181c0168fd6bbf8
atul-shimpi <shimpiatul@hotmail.com>
2015-11-29 06:04:38 +0530

    Update place property of visitor_location

--- app/models/visitor_location.rb
+++ app/models/visitor_location.rb
@@ -2,6 +2,9 @@ class VisitorLocation < ActiveRecord::Base
   belongs_to(:visitor)
 
   def place
-    country_name + ', ' + region_name + ', ' + city
+    place_ = country_name
+    place_ += ',' + region_name if region_name.size > 0
+    place_ += ',' + city if city.size > 0
+    place_
   end
 end
