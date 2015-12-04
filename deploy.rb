8025c19adb54fd129ac5105da191d3a992b0e987
atul-shimpi <shimpiatul@hotmail.com>
2015-12-04 05:45:28 +0530

    Add deploy script

--- deploy.rb
+++ deploy.rb
@@ -1,7 +1,8 @@
+#!/usr/bin/env ruby
 puts 'Enter comment'
 comment = gets
 
 system "git add ."
 system "git commit -m '#{comment}'"
-system "git push origin master"
-
+system 'git push origin master'
+system 'git push heroku master'
