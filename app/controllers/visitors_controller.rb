6f2cd1827b51c7bbb36cac59f0651ff23936d47f
atul-shimpi <shimpiatul@hotmail.com>
2015-12-11 01:32:56 +0530

    Update page navigation for visitors

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,16 @@
 class VisitorsController < ApplicationController
 	def index
+    params[:page_no] = 1
+    page
+=begin
     @visitors = Visitor.all_important.reverse_order!
-
+    @pages = 45
     respond_to do |format|
       format.html { render :index }
 		  format.xml { render xml: @visitors}
       format.json { render json: @visitors }
     end
+=end
   end
 
   def page
@@ -14,7 +18,6 @@ class VisitorsController < ApplicationController
 
     end_index = page_no * 15
     start_index = end_index - 15
-    #@visitors = Visitor.all_important.reverse_order![start_index...end_index]
     @visitors = Visitor.all_important[start_index...end_index]
 
     @pages = pages(Visitor.all_important.count, 15)
