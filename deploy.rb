1c3a320e288f27250718cfa694aa3bcae2a20192
atul-shimpi <shimpiatul@hotmail.com>
2015-12-11 01:05:11 +0530

    Add page navigation for visitors page

--- deploy.rb
+++ deploy.rb
@@ -2,6 +2,7 @@
 puts 'Enter comment'
 comment = gets
 
+system "rake assets:precompile RAILS_ENV=production"
 system "git add ."
 system "git commit -m '#{comment}'"
 system 'git push origin master'
