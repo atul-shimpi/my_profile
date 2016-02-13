a9b9b2eff3de23396e605e83333437d288700ebe
atul-shimpi <shimpiatul@hotmail.com>
2016-02-13 21:15:40 +0530

    Enable visitor saving. Dont save if its already exists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,6 +14,22 @@ class HomeController < ApplicationController
   end
 
   def index
+    # Save visitor to database
+    # if web server is behind proxy/NAT, then HTTP_X_FORWARDED_FOR contains the IP of origin
+    # if not then REMOTE_HOST contains the IP of origin
+    visitor_host_ip = request.headers.include?(:HTTP_X_FORWARDED_FOR) ?
+                      request.headers[:HTTP_X_FORWARDED_FOR] :
+                      request.headers[:REMOTE_HOST]
+
+    puts 'Curent visitor ' + visitor_host_ip
+    unless Visitor.exists?(["remote_ip = ?", visitor_host_ip])
+      Visitor.create( remote_ip: visitor_host_ip,
+                      remote_host: request.headers[:REMOTE_IP].nil? ? 'NA': request.headers[:REMOTE_IP],
+                      server_name: request.headers[:SERVER_NAME],
+                      is_important: !ENV['NOT_IMPORTANT_VISITORS_IPs'].split.include?(visitor_host_ip)
+                    )
+
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,17 @@
-ActiveRecord::Schema.define(version: 20151128203142) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151215172328) do
 
   create_table "my_app_uis", force: :cascade do |t|
     t.integer "my_app_id",  limit: 4
@@ -13,7 +26,6 @@ ActiveRecord::Schema.define(version: 20151128203142) do
     t.string "description",  limit: 2014
     t.string "technologies", limit: 255
   end
-
 
   create_table "visitor_locations", force: :cascade do |t|
     t.integer "visitor_id",     limit: 4
@@ -44,6 +56,10 @@ ActiveRecord::Schema.define(version: 20151128203142) do
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.boolean  "is_important",             default: true
+  end
+
+  create_table "visitors_backups", force: :cascade do |t|
+    t.string "ip", limit: 255
   end
 
 end
