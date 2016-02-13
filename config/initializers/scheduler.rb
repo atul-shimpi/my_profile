8f3b86d2e8cc5f8e4b896a1ffd2345adc7dae382
atul-shimpi <shimpiatul@hotmail.com>
2016-02-13 21:19:41 +0530

    Save visitor only once by ip

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,10 +3,10 @@ require 'rake'
 #scheduler = Rufus::Scheduler.new(:lockfile => ".rufus-scheduler.lock")
 
 scheduler = Rufus::Scheduler.new
-#unless scheduler.down?
-  #scheduler.every("20m") do
-    #Rails.logger.info('Scheduler : Getting IP Location started')
-    #CreateVisitorLocation.create_all
-    #Rails.logger.info('Scheduler : Getting IP Location completed')
-  #end
-#end
+unless scheduler.down?
+  scheduler.every("20m") do
+    Rails.logger.info('Scheduler : Getting IP Location started')
+    CreateVisitorLocation.create_all
+    Rails.logger.info('Scheduler : Getting IP Location completed')
+  end
+end
--- deploy.rb
+++ deploy.rb
@@ -2,7 +2,7 @@
 puts 'Enter comment'
 comment = gets
 
-system "rake assets:precompile RAILS_ENV=production"
+system "bundle exec rake assets:precompile RAILS_ENV=production"
 system "git add ."
 system "git commit -m '#{comment}'"
 system 'git push origin master'
